2f5154a88f734ee327673f564860cf6f568bf2f7
armaanshoaib <76841030+armaanshoaib@users.noreply.github.com>
2025-03-11 10:21:23 +0530

    Add files via upload
    rename this "config.template.js" to "config.js" & add your api key

---
+++ config.template.js
@@ -0,0 +1,8 @@
+export const CONFIG = {
+  // OPENAI_API_KEY: 'YOUR_OPENAI_API_KEY', // Replace with your API key
+  // provider: 'openai',
+  // model: 'gpt-4o-mini', // Primary model to use
+  // backupModel: 'gpt-3.5-turbo', // Fallback model for rate limits/overload
+
+
+};
